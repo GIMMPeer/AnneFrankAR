c4e7e4a888ead74b11b16c23ebf9d867cf4d1f0e
SoDaBearJess <jessna.debsword@gmail.com>
2022-03-21 11:17:13 -0700

    Basic AR Portal

---
+++ AnneFrankAR/AnneFrankAR/Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  Extensions.swift
+//  AnneFrankAR
+//
+//  Created by Jessna Rodriguez on 3/21/22.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+var width : CGFloat = 0.03
+var height : CGFloat = 1
+var length : CGFloat = 1
+
+var doorLength : CGFloat = 0.3
+
+func createBox(isDoor : Bool) -> SCNNode {
+    let node = SCNNode()
+    
+    //Add First Box
+    let firstBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    firstBox.firstMaterial?.diffuse.contents = UIColor.blue
+    
+    let firstBoxNode = SCNNode(geometry: firstBox)
+    firstBoxNode.renderingOrder = 200
+    
+    node.addChildNode(firstBoxNode)
+    
+    //Add Masked Box
+    let maskedBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    maskedBox.firstMaterial?.diffuse.contents = UIColor.blue
+    maskedBox.firstMaterial?.transparency = 0.00001
+    
+    let maskedBoxNode = SCNNode(geometry: maskedBox)
+    maskedBoxNode.renderingOrder = 100
+    maskedBoxNode.position = SCNVector3.init(width, 0, 0)
+    
+    node.addChildNode(maskedBoxNode)
+    return node
+}
+
+extension FloatingPoint {
+    var degreesToRadians : Self {
+        return self * .pi / 180
+    }
+    
+    var radiansToDegrees : Self {
+        return self * 180 / .pi
+    }
+}
--- AnneFrankAR/AnneFrankAR/ViewController.swift
+++ AnneFrankAR/AnneFrankAR/ViewController.swift
@@ -7,18 +7,120 @@
 
 import UIKit
 import RealityKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
     
-    @IBOutlet var arView: ARView!
+    //@IBOutlet var arView: ARView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! PosterInteractiveExample.loadBox()
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! PosterInteractiveExample.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.scene = scene
+        setupScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func setupScene() {
+        let node = SCNNode()
+        node.position = SCNVector3.init(0, 0, 0)
+        
+        let leftWall = createBox(isDoor: false)
+        leftWall.position = SCNVector3.init((-length / 2) + width, 0, 0)
+        leftWall.eulerAngles = SCNVector3.init(0, 180.0.degreesToRadians, 0)
+        
+        let rightWall = createBox(isDoor: false)
+        rightWall.position = SCNVector3.init((length / 2) - width, 0, 0)
+        
+        let topWall = createBox(isDoor: false)
+        topWall.position = SCNVector3.init(0, (height / 2) - width, 0)
+        topWall.eulerAngles = SCNVector3.init(0, 0, 90.0.degreesToRadians)
+        
+        let bottomWall = createBox(isDoor: false)
+        bottomWall.position = SCNVector3.init(0, (-height / 2) + width, 0)
+        bottomWall.eulerAngles = SCNVector3.init(0, 0, -90.0.degreesToRadians)
+        
+        let backWall = createBox(isDoor: false)
+        backWall.position = SCNVector3.init(0, 0, (-length / 2) + width)
+        backWall.eulerAngles = SCNVector3.init(0, 90.0.degreesToRadians, 0)
+        
+        let leftDoorSide = createBox(isDoor: true)
+        leftDoorSide.position = SCNVector3.init((-length / 2 + width) + (doorLength / 2), 0, (length / 2) - width)
+        leftDoorSide.eulerAngles = SCNVector3.init(0, -90.0.degreesToRadians, 0)
+        
+        let rightDoorSide = createBox(isDoor: true)
+        rightDoorSide.position = SCNVector3.init((length / 2 - width) - (doorLength / 2), 0, (length / 2) - width)
+        rightDoorSide.eulerAngles = SCNVector3.init(0, -90.0.degreesToRadians, 0)
+        
+        //create light
+        let light = SCNLight()
+        light.type = .spot
+        light.spotInnerAngle = 70
+        light.spotOuterAngle = 120
+        light.zNear = 0.00001
+        light.zFar = 5
+        light.castsShadow = true
+        light.shadowRadius = 200
+        light.shadowColor = UIColor.black.withAlphaComponent(0.3)
+        light.shadowMode = .deferred
+        let constraint = SCNLookAtConstraint(target: bottomWall)
+        constraint.isGimbalLockEnabled = true
+        
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3.init(0, (height / 2) - width, 0)
+        lightNode.constraints = [constraint]
+        node.addChildNode(lightNode)
+        
+        node.addChildNode(leftWall)
+        node.addChildNode(rightWall)
+        node.addChildNode(topWall)
+        node.addChildNode(bottomWall)
+        node.addChildNode(backWall)
+        node.addChildNode(leftDoorSide)
+        node.addChildNode(rightDoorSide)
 
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
     }
 }
