753f5d0b1f93ab5495f56224dfbd216439d3ecc4
SoDaBearJess <jessna.debsword@gmail.com>
2022-04-01 10:07:13 -0700

    commented code

--- AnneFrankAR/AnneFrankAR/Extensions.swift
+++ AnneFrankAR/AnneFrankAR/Extensions.swift
@@ -16,44 +16,55 @@ var length : CGFloat = 2
 var doorLength : CGFloat = 0.6
 
 func createBox(isDoor : Bool, img : String) -> SCNNode {
+    //root node that will be added to the scene
     let node = SCNNode()
     
-    //Add First Box
+    //Add First Box, this is the textured inside wall
     let firstBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    //add the image texture
     firstBox.firstMaterial?.diffuse.contents = UIImage(named: img)
     
+    //the box object needs a node to attach to in the actual scene
     let firstBoxNode = SCNNode(geometry: firstBox)
     firstBoxNode.renderingOrder = 200
-    
+    //add the box node to the root node
     node.addChildNode(firstBoxNode)
-    
-    //Add Masked Box
+//--------------------------------------------------------------------------------------------
+    //Add Masked Box, this is the outside occluded box so things look hidden
     let maskedBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
     maskedBox.firstMaterial?.diffuse.contents = UIColor.blue
+    //the transparency is the main thing needed here, it's what makes the box invisible and occluded
     maskedBox.firstMaterial?.transparency = 0.00001
     
+    //the invisible wall needs a node to attach to in the actual scene
     let maskedBoxNode = SCNNode(geometry: maskedBox)
     maskedBoxNode.renderingOrder = 100
     maskedBoxNode.position = SCNVector3.init(width, 0, 0)
     
+    //add the wall node to the root node
     node.addChildNode(maskedBoxNode)
     return node
 }
 
 func createPoster(image: String) -> SCNNode {
+    //root node that will be added to the scene
     let node = SCNNode()
     
+    //creates box with poster image texture
     let poster = SCNBox(width: 0.03, height: 0.5, length: 0.3, chamferRadius: 0)
     poster.firstMaterial?.diffuse.contents = UIImage(named: image)
     
+    //the poster needs a node to attach to in the actual scene
     let posterNode = SCNNode(geometry: poster)
     posterNode.renderingOrder = 300
     
+    //add the poster node to the root node
     node.addChildNode(posterNode)
     
     return node
 }
 
+//couple helper functions for rotation
 extension FloatingPoint {
     var degreesToRadians : Self {
         return self * .pi / 180
--- AnneFrankAR/AnneFrankAR/ViewController.swift
+++ AnneFrankAR/AnneFrankAR/ViewController.swift
@@ -13,21 +13,20 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     //@IBOutlet var arView: ARView!
+    //Connection to the AR Scene View
     @IBOutlet weak var sceneView: ARSCNView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        // Load the "Box" scene from the "Experience" Reality File
-//        let boxAnchor = try! PosterInteractiveExample.loadBox()
-//
-//        // Add the box anchor to the scene
-//        arView.scene.anchors.append(boxAnchor)
+        //init things for scene
         sceneView.delegate = self
+        //show statistics shows framerate in corner, could probably be removed in future
         sceneView.showsStatistics = true
         let scene = SCNScene()
         sceneView.scene = scene
+        //setupScene is the function that builds the AR portal
         setupScene()
     }
     
@@ -48,11 +47,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func setupScene() {
-        //Rendering order must be above 100 in order to be properly hidden in the portal
+        //**Rendering order must be above 100 in order to be properly hidden in the portal
         
+        //node is the spawn point of everything in the scene
         let node = SCNNode()
         node.position = SCNVector3.init(0, 0, 0)
         
+        //each wall calls createBox() which is in Extensions
         let leftWall = createBox(isDoor: false, img: "art.scnassets/Wall Textures/Wall_1.png")
         leftWall.position = SCNVector3.init((-length / 2) + width, 0, 0)
         leftWall.eulerAngles = SCNVector3.init(0, 180.0.degreesToRadians, 0)
@@ -80,25 +81,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         rightDoorSide.position = SCNVector3.init((length / 2 - width) - (doorLength / 2), 0, (length / 2) - width)
         rightDoorSide.eulerAngles = SCNVector3.init(0, -90.0.degreesToRadians, 0)
         
+        //This is not a wall, it's a poster. Works very similar to the walls though
+        //createPoster() is also in Extensions
         let posterTest = createPoster(image: "art.scnassets/Poster_Base_AR.png")
         posterTest.position = SCNVector3.init(0, 0, (-length / 2) + width * 2)
         posterTest.eulerAngles = SCNVector3.init(0, 90.0.degreesToRadians, 0)
         
+        //This is accessing the AmericanPillar.scn file which has 2 pillar objects in it
         let subScene = SCNScene(named: "art.scnassets/AmericanPillar.scn")!
         
+        //This is accessing the scn file and then the specific pillar object called "Cylinder"
         let amer = subScene.rootNode.childNode(withName: "Cylinder", recursively: true)!
-      //  amer.renderingOrder=200
         amer.position = SCNVector3.init(0.75, -0.1, (-length / 2) + width * 3)
         amer.eulerAngles = SCNVector3.init(0, -90.0.degreesToRadians, 0)
         
+        //This is accessing the scn file and then the specific pillar object called "Cylinder2"
         let germ = subScene.rootNode.childNode(withName: "Cylinder2", recursively: true)!
-        //germ.renderingOrder=200
         germ.position = SCNVector3.init(-0.75, -0.1, (-length / 2) + width * 3)
         germ.eulerAngles = SCNVector3.init(0, -90.0.degreesToRadians, 0)
 
         
-        //create light
+        //create light, otherwise the portal would be black
         let light = SCNLight()
+        //omni has worked the best on image textures, but could be better
         light.type = .omni
         light.spotInnerAngle = 70
         light.spotOuterAngle = 120
@@ -109,15 +114,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         light.intensity = 400
         light.shadowColor = UIColor.black.withAlphaComponent(0.7)
         light.shadowMode = .forward
+        //This points the light in a specific direction
         let constraint = SCNLookAtConstraint(target: bottomWall)
         constraint.isGimbalLockEnabled = true
         
+        //This is what actually places the light in the scene
         let lightNode = SCNNode()
         lightNode.light = light
         lightNode.position = SCNVector3.init(0, 4, 0)
         lightNode.constraints = [constraint]
         node.addChildNode(lightNode)
         
+        //node is the main scene node in the center of the scene
+        //If you add something and it's not showing up in the scene, chances are you need to add it to node
         node.addChildNode(leftWall)
         node.addChildNode(rightWall)
         node.addChildNode(topWall)
@@ -131,7 +140,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.addChildNode(amer)
         node.addChildNode(germ)
         
-        
+        //This is the final step, officially adding node to the scene itself
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
