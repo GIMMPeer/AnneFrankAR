29a2af6b654ab46ff0a75a00c47263ffc88486b3
Ivar <60413917+ivar5@users.noreply.github.com>
2022-04-08 12:55:21 -0600

    cleaned up, videos need more testing

---
+++ AnneFrankAR/AnneFrankAR/VideoPlayer.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  AnneFrankAR
+//
+//  Created by Tri Nguyen on 3/4/22.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! PosterInteractiveExample.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        PlayVideo()
+    }
+        
+        private func PlayVideo(){
+            //might have to convert the mp4 videos to m4v - Ivar
+            //There are also 3 videos of speeches and one of the crowd at a rally
+            
+            //Need to set up a button in the main scene somewhere to open the video view
+            //Only going to set up a single video for now as proof. Can easily set up the
+            //other videos later once the UI is more fleshed out. May have to find more.
+            
+            guard let path = Bundle.main.path(forResource: "video1_Speech", ofType: "mp4") else{
+                debugPrint("video1_Speech.mp4 not found")
+                return
+            }
+            let player = AVPlayer(url:URL(fileURLWithPath: path))
+            let playerController = AVPlayerViewController()
+            playerController.player = player
+            present(playerController, animated: true){
+                player.play()
+            }
+    }
+}
