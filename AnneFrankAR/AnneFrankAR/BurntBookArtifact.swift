5fe3a8e6ce7c55313f2f796b2ce0b64b0a0bd53b
WondrousNights <77310193+WondrousNights@users.noreply.github.com>
2022-05-23 12:12:38 -0600

    Functional
    Looks Bad

--- AnneFrankAR/AnneFrankAR/BurntBookArtifact.swift
+++ AnneFrankAR/AnneFrankAR/BurntBookArtifact.swift
@@ -20,28 +20,40 @@ class BurntBookArtifact: UIViewController, SCNSceneRendererDelegate
     
     @IBOutlet weak var bookText: UILabel!
     
+    @IBOutlet weak var bookButton1: UIButton!
     
-  
+    @IBOutlet weak var bookButton2: UIButton!
+    
+    @IBOutlet weak var bookButton3: UIButton!
+    
+    @IBOutlet weak var bookButton4: UIButton!
+    
+    @IBOutlet weak var bookButton5: UIButton!
     
     @IBAction func book1(_ sender: Any) {
         setupReading(bookNum: 1)
+        bookButton1.isHidden = true
     }
     
     @IBAction func book2(_ sender: Any) {
         setupReading(bookNum: 2)
+        bookButton2.isHidden = true
     }
     
     @IBAction func book3(_ sender: Any) {
         setupReading(bookNum: 3)
+        bookButton3.isHidden = true
 
     }
     
     @IBAction func book4(_ sender: Any) {
         setupReading(bookNum: 4)
+        bookButton4.isHidden = true
     }
     
     @IBAction func book5(_ sender: Any) {
         setupReading(bookNum: 5)
+        bookButton5.isHidden = true
     }
     
     override func viewDidLoad() {
@@ -132,13 +144,34 @@ class BurntBookArtifact: UIViewController, SCNSceneRendererDelegate
        if(bookNum == 1)
         {
            bookText.text = "The first panacea for a mismanaged nation is inflation of the currency; the second is war. Both bring a temporary prosperity; both bring a permanent ruin. But both are the refuge of political and economic opportunists. —Notes on the Next War, Esquire magazine, Ernest Hemingway, September 1935"
-           bookText.sizeToFit()
            
-           let timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
-               self.bookText.isHidden = true
-               self.bookStack.isHidden = false
+           
+           
                
            }
+        if(bookNum == 2)
+        {
+            bookText.text = "I am young, I am twenty years old; yet I know nothing of life but despair, death, fear, and fatuous superficiality cast over an abyss of sorrow. I see how peoples are set against one another, and in silence, unknowingly, foolishly, obediently, innocently slay one another."
+            
+        }
+        if(bookNum == 3)
+        {
+            bookText.text = "We will grind you revolutionists down under our heel, and we shall walk upon your faces. The world is ours, we are its lords, and ours it shall remain. As for the host of labor, it has been in the dirt since history began, and I read history aright. And in the dirt it shall remain so long as I and mine and those that come after us have the power. There is the word. It is the king of words—Power. Not God, not Mammon, but Power. Pour it over your tongue till it tingles with it. Power."
+        }
+        if(bookNum == 4)
+        {
+            bookText.text = "Is it not astonishing that, in the course of history, all human types except [the soulful human who possesses fantasy] have been in power?...Instead of expecting such a person to come along, we must expect gas warfare! And the culprit will be the philistine nature of the political and economical world powers. Everything evil or stupid in this world is not supernatural destiny, but rather a deadly form of lack of fantasy..."
+        }
+        if(bookNum == 5)
+        {
+            bookText.text = "But one who understands will not judge, and will have no pride. Before him I shall not be ashamed. Whoever has found himself can never again lose anything in this world. He who has grasped the human in himself understands all mankind."
+        }
+        
+        bookText.numberOfLines = 5
+        bookText.sizeToFit()
+        let timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+            self.bookText.isHidden = true
+            self.bookStack.isHidden = false
        }
     }
 }
