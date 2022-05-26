906814f7cdb3bd99a43f877ec32b1560c80d59f6
WondrousNights <77310193+WondrousNights@users.noreply.github.com>
2022-05-26 12:08:30 -0600

    Rotations work
    Its in radians!

--- AnneFrankAR/AnneFrankAR/ViewController.swift
+++ AnneFrankAR/AnneFrankAR/ViewController.swift
@@ -317,8 +317,10 @@ class ViewController: UIViewController, ARSCNViewDelegate, CBPeripheralDelegate,
             propagandaLesson.position = SCNVector3.init(-0.50, -0.5, -0.75)
             
             let person = subScene.rootNode.childNode(withName: "stan", recursively: true)!
-            person.position = SCNVector3.init(0, -0.5, -0.75)
-            person.eulerAngles = SCNVector3.init(136.783, 0, 46.398)
+            person.position = SCNVector3.init(0, -0.95, -0.75)
+            person.eulerAngles = SCNVector3.init(0, Double.pi / 16, 0)
+            
+            
             
             node.addChildNode(propagandaLesson)
             node.addChildNode(quiz)
