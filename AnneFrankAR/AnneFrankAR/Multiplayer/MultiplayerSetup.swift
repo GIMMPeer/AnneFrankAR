5a296d700f0daac9b8bfb886cc4c494aeeb4ce54
Lucas Greer <lucasgreer2@gmail.com>
2022-05-31 11:56:49 -0600

    Replaced my code with this guy's handler

--- AnneFrankAR/AnneFrankAR/Multiplayer/MultiplayerSetup.swift
+++ AnneFrankAR/AnneFrankAR/Multiplayer/MultiplayerSetup.swift
@@ -1,41 +1,94 @@
 //
-//  MultiplayerSetup.swift
-//  AnneFrankAR
+//  Matchmaking.swift
+//  GameCenterMultiplayer
 //
-//  Created by Lucas Greer on 5/31/22.
+//  Created by Pedro Contine on 29/06/20.
 //
-
 import Foundation
 import GameKit
 
-func initializeMultiplayer(view: UIViewController) {
-    GKLocalPlayer.local.authenticateHandler = { viewController, error in
-        if let viewController = viewController {
-            // Present the view controller so the player can sign in.
-            view.present(viewController, animated: true)
-            return
+protocol GameCenterHelperDelegate: class {
+    func didChangeAuthStatus(isAuthenticated: Bool)
+    func presentGameCenterAuth(viewController: UIViewController?)
+    func presentMatchmaking(viewController: UIViewController?)
+    func presentGame(match: GKMatch)
+}
+
+final class GameCenterHelper: NSObject, GKLocalPlayerListener {
+    weak var delegate: GameCenterHelperDelegate?
+    
+    private let minPlayers: Int = 2
+    private let maxPlayers: Int = 3
+    private let inviteMessage = "Anne Frank Memorial invites you to open their app"
+    
+    private var currentVC: GKMatchmakerViewController?
+    
+    var isAuthenticated: Bool {
+        return GKLocalPlayer.local.isAuthenticated
+    }
+    
+    func authenticatePlayer() {
+        GKLocalPlayer.local.authenticateHandler = { (gameCenterAuthViewController, error) in
+            self.delegate?.didChangeAuthStatus(isAuthenticated: self.isAuthenticated)
+            
+            guard GKLocalPlayer.local.isAuthenticated else {
+                self.delegate?.presentGameCenterAuth(viewController: gameCenterAuthViewController)
+                return
+            }
+
+            GKLocalPlayer.local.register(self)
         }
-        if error != nil {
-            // Player could not be authenticated.
-            // Disable Game Center in the game.
-            let alert = UIAlertController(title: "Error", message: "\(error)", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-                switch action.style{
-                    case .default:
-                    print("default")
-                    
-                    case .cancel:
-                    print("cancel")
-                    
-                    case .destructive:
-                    print("destructive")
-                    
-                }
-            }))
-            view.present(alert, animated: true)
+    }
+    
+    func presentMatchmaker(withInvite invite: GKInvite? = nil) {
+        guard GKLocalPlayer.local.isAuthenticated,
+              let vc = createMatchmaker(withInvite: invite) else {
             return
         }
         
-        // Perform any other configurations as needed (for example, access point).
+        currentVC = vc
+        vc.matchmakerDelegate = self
+        delegate?.presentMatchmaking(viewController: vc)
+    }
+    
+    private func createMatchmaker(withInvite invite: GKInvite? = nil) -> GKMatchmakerViewController? {
+        
+        //If there is an invite, create the matchmaker vc with it
+        if let invite = invite {
+            return GKMatchmakerViewController(invite: invite)
+        }
+        
+        return GKMatchmakerViewController(matchRequest: createRequest())
+    }
+    
+    private func createRequest() -> GKMatchRequest {
+        let request = GKMatchRequest()
+        request.minPlayers = minPlayers
+        request.maxPlayers = maxPlayers
+        request.inviteMessage = inviteMessage
+        
+        return request
+    }
+}
+
+
+extension GameCenterHelper: GKMatchmakerViewControllerDelegate {
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
+        viewController.dismiss(animated: true)
+        delegate?.presentGame(match: match)
+    }
+    
+    func player(_ player: GKPlayer, didAccept invite: GKInvite) {
+        currentVC?.dismiss(animated: true, completion: {
+            self.presentMatchmaker(withInvite: invite)
+        })
+    }
+    
+    func matchmakerViewControllerWasCancelled(_ viewController: GKMatchmakerViewController) {
+        viewController.dismiss(animated: true)
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFailWithError error: Error) {
+        print("Matchmaker vc did fail with error: \(error.localizedDescription).")
     }
 }
